720cac13a5205462e7118f34e2fdf99b9c5bc5b1
Alejandro Quadri <alejandroquadri@gmail.com>
2016-03-16 19:01:42 -0300

    Terminado el primer endpoint

--- server/app.js
+++ server/app.js
@@ -8,8 +8,17 @@ app.use(express.static (__dirname+"/../client") );
 //clientes todos los archivos estaticos, tiene que
 //volver para atras y ahi aclarar que es client.
 
-app.listen(8181, ()=>console.log("Listening on 8181"));
+app.get("/sports", (request, response)=>{
+  response.json(["Cycling","Wheightlifting"])
+});
 
+// lo siguiente seria sin usar el bendito ES2015
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+
+app.listen(8181, ()=>console.log("Listening on 8181"));
 /*
 esta usando el js nuevo ES2015, seria lo mismo que escribir
 
